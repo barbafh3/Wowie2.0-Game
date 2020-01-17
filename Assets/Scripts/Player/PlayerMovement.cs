0d26d8c338e255e5c5549ce78afcdf3fa887fa8b
Gilberto Timotheo Junior <barbafh@gmail.com>
2020-01-17 10:30:40 -0300

    Adicionado movimento ao player

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+  public float speed;
+
+  public Rigidbody2D rb;
+
+  public Animator animator;
+
+  public float jumpHeight;
+
+  bool _isJumping = false;
+
+
+  void Update()
+  {
+    var horizontal = Input.GetAxisRaw("Horizontal");
+
+    Move(horizontal);
+
+    if (Input.GetKeyDown(KeyCode.Space) && !_isJumping)
+    {
+      Jump();
+    }
+  }
+
+  void Move(float horizontal)
+  {
+    var movement = new Vector2(horizontal * speed, rb.velocity.y);
+    rb.velocity = movement;
+  }
+
+  public void Jump()
+  {
+    rb.velocity = Vector2.zero;
+    _isJumping = true;
+    animator.SetBool("isFalling", false);
+    rb.AddForce(Vector2.up * jumpHeight);
+  }
+}
