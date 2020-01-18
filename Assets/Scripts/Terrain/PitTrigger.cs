f099709079bccd5fe46e17b8b5ef8ac2b04f77c6
Gilberto Timotheo Junior <barbafh@gmail.com>
2020-01-18 12:10:12 -0300

    Adicionados triggers nos espetos e no buraco para respawn

---
+++ Assets/Scripts/Terrain/PitTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PitTrigger : MonoBehaviour
+{
+  public Transform respawnTransform;
+
+  void OnTriggerEnter2D(Collider2D other)
+  {
+    if (other.CompareTag("Player"))
+    {
+      other.GetComponent<PlayerMovement>().rb.velocity = Vector2.zero;
+      other.transform.position = respawnTransform.position;
+    }
+  }
+}
