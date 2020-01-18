675909428340ae52be2e462b72846af5f0829e64
Marcial Lincoln <marcial.snake@gmail.com>
2020-01-18 14:32:04 -0300

    Enemy Skeleton e Archer

---
+++ Assets/Scripts/Enemy/Skeleton.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+
+public class Skeleton : EnemyBase {
+
+    public float distanceForFollow;
+    public float distanceForAttack;
+    public float countdown = 1;
+    public bool lookRight = true;
+
+    private Vector3 startPos;
+    private Animator anim;
+    private float timer;
+    private float speedCurrent = 0;
+    private bool inAttack;
+
+    private new void Start () {
+        base.Start ();
+
+        anim = GetComponent<Animator> ();
+        startPos = transform.position;
+
+        Flip();
+
+    }
+
+    private void Update () {
+        var newTarget = new Vector3 (player.position.x, transform.position.y, transform.position.z);
+        var distance = Vector3.Distance (transform.position, newTarget);
+
+        anim.SetFloat ("speed", speedCurrent);
+
+        timer += Time.deltaTime;
+
+        if (death) return;
+
+        if (distance <= distanceForAttack) {
+            if (timer >= countdown) {
+                timer = 0;
+                anim.SetTrigger ("attack");
+                speedCurrent = 0;
+                inAttack = true;
+            }
+
+        } else if (distance <= distanceForFollow && !inAttack) {            
+            Flip();
+            speedCurrent = speed;
+            transform.position = Vector2.MoveTowards (transform.position, newTarget, speedCurrent * Time.deltaTime);
+
+        } else if (distance > distanceForFollow) {
+            transform.position = Vector2.MoveTowards (transform.position, startPos, speedCurrent * Time.deltaTime);
+        }
+    }
+
+    public void DisableAttack () => inAttack = false;
+
+    protected override void OnDeath () {
+        speedCurrent = 0;
+        anim.SetTrigger ("death");
+        Destroy (gameObject, 1.1f);
+    }
+
+
+    private void Flip() {
+        if (transform.position.x > player.position.x && !lookRight || transform.position.x < player.position.x && lookRight) {
+                lookRight = !lookRight;
+
+                transform.rotation = (lookRight) ? Quaternion.Euler (0, 180, 0) : Quaternion.Euler (0, 0, 0);
+            }
+    }
+}
