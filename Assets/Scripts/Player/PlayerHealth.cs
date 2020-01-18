c27bd1f2095e7fb3d68096292d6afa99f1303a79
Gilberto Timotheo Junior <barbafh@gmail.com>
2020-01-18 09:53:32 -0300

    Adicionada vida do player

---
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+  public float maxHealth, startingHealth;
+
+  public FloatVariable currentHealth;
+
+  public GameEvent healthChangeEvent;
+
+  void Start()
+  {
+    currentHealth.Value = startingHealth;
+  }
+
+  public void AddHealth(float amount)
+  {
+    currentHealth.Value += amount;
+    healthChangeEvent.Raise();
+  }
+
+  public void RemoveHealth(float amount)
+  {
+    currentHealth.Value -= amount;
+    healthChangeEvent.Raise();
+  }
+}
