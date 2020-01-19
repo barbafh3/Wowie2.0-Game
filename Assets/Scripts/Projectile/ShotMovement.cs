dd5d6f26ab8b6ea9fb6ae9ba3a943f5e5242cbf4
Gilberto Timotheo Junior <barbafh@gmail.com>
2020-01-19 13:59:13 -0300

    Trocado sprite do tiro

--- Assets/Scripts/Projectile/ShotMovement.cs
+++ Assets/Scripts/Projectile/ShotMovement.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ShotMovement : MonoBehaviour {
+public class ShotMovement : MonoBehaviour
+{
 
   public Rigidbody2D rb;
 
@@ -10,33 +11,39 @@ public class ShotMovement : MonoBehaviour {
 
   Transform player;
 
-  void Start () {
-    var playerTransform = GameObject.FindGameObjectWithTag ("Player").transform;
-    player = playerTransform.GetChild (0).transform;
-    Invoke ("Collided", 5f);
+  void Start()
+  {
+    var playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+    player = playerTransform.GetChild(0).transform;
+    Invoke("Collided", 5f);
   }
 
-  void Update () {
-    Move ();
+  void Update()
+  {
+    Move();
   }
 
-  void Move () {
-    transform.Translate (-Vector2.right * speed * Time.deltaTime);
+  void Move()
+  {
+    transform.Translate(-Vector2.right * speed * Time.deltaTime);
     var x = transform.position.x;
     var y = player.position.y;
-    transform.position = new Vector2 (x, y);
+    transform.position = new Vector2(x, y);
   }
 
-  public void Collided () {
+  public void Collided()
+  {
     // Mostra particulas
-    Destroy (gameObject);
+    Destroy(gameObject);
   }
 
-  private void OnTriggerEnter2D (Collider2D other) {
-    var enemy = other.GetComponent<EnemyBase> ();
+  private void OnTriggerEnter2D(Collider2D other)
+  {
+    var enemy = other.GetComponent<EnemyBase>();
 
-    if (enemy != null) {
-      enemy.ApplyDamage (1);
+    if (enemy != null)
+    {
+      enemy.ApplyDamage(1);
       Destroy(gameObject);
     }
 
