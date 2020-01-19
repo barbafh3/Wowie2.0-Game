9eb7ac0b5d2eae1667f34a73b4751744b8e94fa6
Marcial Lincoln <marcial.snake@gmail.com>
2020-01-19 09:03:20 -0300

    Som do Switch adicionado

--- Assets/Scripts/Enviroment/SwichController.cs
+++ Assets/Scripts/Enviroment/SwichController.cs
@@ -5,24 +5,27 @@ public class SwichController : MonoBehaviour {
     public Transform elevator;
     public Transform A, B;
     public float speed;
+    public AudioClip swAudio;
+    public AudioClip spAudio;
 
     public Sprite[] sprites;
 
     private bool onOff = false;
     private Vector3 target;
     private SpriteRenderer spriteRenderer;
+    private AudioSource audioSource;
 
     private void Start () {
         spriteRenderer = GetComponent<SpriteRenderer> ();
+        audioSource = GetComponent<AudioSource>();
 
         target = A.position;
+        elevator.position = A.position;
+        spriteRenderer.sprite = sprites[0];
     }
 
     private void Update () {
-        elevator.position = Vector3.MoveTowards (elevator.position, target, speed * Time.deltaTime);
-
-        if (Input.GetMouseButtonDown (0))
-            OnOff ();
+        elevator.position = Vector3.MoveTowards (elevator.position, target, speed * Time.deltaTime);        
     }
 
     private void OnTriggerEnter2D (Collider2D other) {
@@ -39,6 +42,9 @@ public class SwichController : MonoBehaviour {
                 spriteRenderer.sprite = sprites[0];
                 target = A.position;
             }
+
+            audioSource.PlayOneShot(swAudio);
+            audioSource.PlayOneShot(spAudio);
 
             Destroy(other.gameObject);
         }
